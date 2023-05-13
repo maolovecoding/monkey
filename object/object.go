6b23d55bff230a6d019488006e0cfc9ca1c01296
codermao <1339065735@qq.com>
2023-05-13 16:55:52 +0800

    整数

--- object/object.go
+++ object/object.go
@@ -1,9 +1,28 @@
 package object
 
-type ObjectType string
+import "fmt"
+
+type ObjectType string // 对象类型
+
+const (
+	INTEGER_OBJ = "INTEGER"
+)
 
 // 对象表示
 type Object interface {
 	Type() ObjectType // 类型
 	Inspect() string  // 检查
+}
+
+// 整数
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
 }
