247c0d12ac6200bf25aad574beca71f0c2c6f540
codermao <1339065735@qq.com>
2023-06-23 13:09:43 +0800

    hash 字面量求值

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -90,6 +90,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return index
 		}
 		return evalIndexExpression(left, index)
+	case *ast.HashLiteral:
+		return evalHashLiteral(node, env)
 	case *ast.BlockStatement:
 		return evalBlockStatement(node, env)
 	case *ast.IfExpression:
@@ -355,4 +357,31 @@ func evalArrayIndexExpression(array, index object.Object) object.Object {
 		return NULL // 越界
 	}
 	return arrayObject.Elements[idx]
+}
+
+// 处理hash数据结构
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+	for keyNode, valueNode := range node.Pairs {
+		key := Eval(keyNode, env)
+		if isError(key) {
+			return key
+		}
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return newError("unusable as hash key: %s", key.Type())
+		}
+		value := Eval(valueNode, env)
+		if isError(value) {
+			return value
+		}
+		hashed := hashKey.HashKey()
+		pairs[hashed] = object.HashPair{
+			Key:   key,
+			Value: value,
+		}
+	}
+	return &object.Hash{
+		Pairs: pairs,
+	}
 }
